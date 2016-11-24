ffe3bb7528ee34ccc7c658469a19a832034d1e1b
cleverUtd <349856491@qq.com>
2016-11-24 23:10:01 +0800

    update bash

--- rsync.sh
+++ rsync.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+echo "hexo clean..."
+hexo clean 
+echo "hexo clean done."
+
+echo "hexo generate..."
+hexo g
+echo "hexo generate done."
+
+echo "hexo deploy..."
+hexo d
+echo "hexo deploy done."
+
+
 ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 echo ${ROOT}
